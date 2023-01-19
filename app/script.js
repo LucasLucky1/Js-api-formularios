73ac34fe5bee2b33667138c697d410c27b4898b0
LucasLucky1 <lucaserickson100@gmail.com>
2023-01-18 23:26:55 -0300

    começando o método async await

--- app/script.js
+++ app/script.js
@@ -1,17 +1,25 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
+// var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(resposta => resposta.json())
+// .then(r => {
     
-        if (r.erro){
-            throw Error('Esse CEP não existe!')
-        }else
-            console.log(r)
-})
-.catch(erro=> console.log(erro))
-.finally(mensagemIndependente=>console.log('Vou aparecer de qlqr forma'))
+//         if (r.erro){
+//             throw Error('Esse CEP não existe!')
+//         }else
+//             console.log(r)
+// })
+// .catch(erro=> console.log(erro))
+// .finally(mensagemIndependente=>console.log('Vou aparecer de qlqr forma'))
 
-// setInterval(fala,5000);
 
-// function fala (){
-//     console.log(consultaCep)
-// }
+async function buscaEndereco(){
+    try{
+        var consultaCep= await fetch('https://viacep.com.br/ws/01001000/json/');
+        var consultaCepConvertida= await consultaCep.json();
+
+        console.log(consultaCepConvertida)
+    }catch(erro){
+        console.log(erro)
+    }
+}
+buscaEndereco()
+
